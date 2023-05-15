99d292104b0e06ecbd4aa5495054a5b7c7492824
WiaanDuvenhage-200307 <wiaanduvenhage.dev@gmail.com>
2023-05-15 11:06:22 +0200

    FIX: Comment
    - Commented out the ConversationCard composable, as it threw an error that my AsyncImage does not work.

--- app/src/main/java/com/example/kinnect/Screens/ConversationsScreen.kt
+++ app/src/main/java/com/example/kinnect/Screens/ConversationsScreen.kt
@@ -19,8 +19,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.example.kinnect.R
 import com.example.kinnect.ui.theme.KinnectTheme
 
@@ -58,36 +56,36 @@ fun ConversationsScreen(modifier: Modifier = Modifier, onNavigateHousehold: () -
 
 }
 
-@Composable
-fun ConversationCard(modifier: Modifier = Modifier){
-
-    Card(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth()) {
-        Column() {
-
-            AsyncImage(
-                model = ImageRequest.Builder(context = LocalContext.current)
-                    .data("https://free4kwallpapers.com/uploads/originals/2021/04/10/android--kotlin-wallpaper.png")
-                    .crossfade(true)
-                    .build(),
-                contentDescription = null,
-                placeholder = painterResource(R.drawable.ic_launcher_foreground),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(194.dp),
-                contentScale = ContentScale.Crop
-            )
-
-            Text(
-                text = "Test",
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
-
-        }
-    }
-}
+//@Composable
+//fun ConversationCard(modifier: Modifier = Modifier){
+//
+//    Card(modifier = Modifier
+//        .padding(8.dp)
+//        .fillMaxWidth()) {
+//        Column() {
+//
+//            AsyncImage(
+//                model = ImageRequest.Builder(context = LocalContext.current)
+//                    .data("https://free4kwallpapers.com/uploads/originals/2021/04/10/android--kotlin-wallpaper.png")
+//                    .crossfade(true)
+//                    .build(),
+//                contentDescription = null,
+//                placeholder = painterResource(R.drawable.ic_launcher_foreground),
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(194.dp),
+//                contentScale = ContentScale.Crop
+//            )
+//
+//            Text(
+//                text = "Test",
+//                modifier = Modifier.padding(16.dp),
+//                style = MaterialTheme.typography.headlineSmall
+//            )
+//
+//        }
+//    }
+//}
 
 @Preview(showSystemUi = true)
 @Composable
